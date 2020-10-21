29b4f58350771d6daf5e1660e153981b0f460fe7
sxs <sunxinshan@163.com>
2020-10-21 13:20:24 +0800

    initial commit

---
+++ hades-webflux/src/main/java/com/hades/webflux/controller/WebFluxApplication.java
@@ -0,0 +1,10 @@
+package com.hades.webflux.controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxApplication {
+
+}
